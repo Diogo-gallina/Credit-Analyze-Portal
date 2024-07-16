8c31bb06b9071095515d0f6524f9145647b3cf31
diogo_gallina <diogogallina20@gmail.com>
2024-07-16 14:44:49 -0300

    feat: create login form

---
+++ credit-analyze-portal/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '../components/LoginForm'
+
+export default function Login() {
+  return (
+    <main className="h-screen bg-zinc-50 flex flex-col gap-10 items-center justify-center">
+      <LoginForm />
+    </main>
+  )
+}
