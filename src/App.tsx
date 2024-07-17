dd102fe9ca5ccb82267cb658b6c112dba8d3268c
diogo_gallina <diogogallina20@gmail.com>
2024-07-17 12:11:30 -0300

    feat: add account state in register route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Upload from './pages/Upload'
+import AccountState from './lib/aws-cognito/accountState'
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<AccountState><Register /></AccountState>} />
         <Route path="/login" element={<Login />} />
         <Route path="/upload" element={<Upload />} />
       </Routes>
