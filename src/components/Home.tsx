5ee30d0b23293dabd37f93eb02586da3db3b7608
diogo_gallina <diogogallina20@gmail.com>
2024-07-18 12:09:31 -0300

    feat: create home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import Contact from "./Contact";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-6 w-full mx-auto bg-white p-12 rounded-lg">
+      <div className="text-center">
+        <h2 className="text-4xl font-bold text-blue-500 mb-4">Credit Analyze</h2>
+        <p className="text-lg text-gray-700 mb-8">
+        Esta é a página inicial da sua aplicação. Navegue pelo site utilizando os links acima.
+        </p>
+      </div>
+
+      <Contact />
+    </div>
+  );
+}
