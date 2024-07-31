16770c2c0cea43271c02e590a1e8753a01910791
diogo_gallina <diogogallina20@gmail.com>
2024-07-31 17:18:45 -0300

    feat: add backend integration on upload route

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -1,43 +1,102 @@
-import { useState } from 'react';
-
+import { useContext, useEffect, useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-
-import { supabase } from '../lib/supabase';
+import userPool from '../lib/aws-cognito/userPool';
+import AccountContext from '../context/AccountContext';
 
 const uploadFormSchema = z.object({
   file: z.array(z.instanceof(File)).refine(
-    (files) => {
-      if (files.length === 0) return false;
-      const file = files[0];
-      return file.size <= 5 * 1024 * 1024;
-    },
+    (files) => files.length > 0 && files[0].size <= 5 * 1024 * 1024,
     {
       message: 'O arquivo pode ter no máximo 5mb',
-    },
+    }
   ),
 });
 
 type UploadFormData = z.infer<typeof uploadFormSchema>;
+type UserAttributes = {
+  [key: string]: string;
+};
 
 export function UploadForm() {
   const [_output, setOutput] = useState('');
   const [fileName, setFileName] = useState('');
+  const [userData, setUserData] = useState({ name: '', email: '', document: '' });
+  const accountContext = useContext(AccountContext);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setValue,
-  } = useForm<UploadFormData>({
+  if (!accountContext) {
+    throw new Error('AccountContext not provided');
+  }
+
+  const { currentSession, token } = accountContext;
+
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm<UploadFormData>({
     resolver: zodResolver(uploadFormSchema),
   });
 
+  useEffect(() => {
+    const cognitoUser = userPool.getCurrentUser();
+
+    if (cognitoUser) {
+      cognitoUser.getSession((err: any, session: any) => {
+        if (err) {
+          console.error('Erro ao obter sessão do usuário:', err);
+          return;
+        }
+
+        cognitoUser.getUserAttributes((err: any, attributes: any) => {
+          if (err || !attributes) {
+            console.error('Erro ao obter atributos do usuário:', err);
+            return;
+          }
+
+          const userAttributes: UserAttributes = {};
+          attributes.forEach((attribute: any) => {
+            userAttributes[attribute.Name] = attribute.Value;
+          });
+
+          setUserData({
+            name: userAttributes['name'] || '',
+            email: userAttributes['email'] || '',
+            document: userAttributes['custom:Document'] || ''
+          });
+        });
+      });
+    }
+  }, []);
+
   async function uploadFile(data: UploadFormData) {
+    if (!token) {
+      console.error('Usuário não autenticado');
+      return;
+    }
+
     const file = data.file[0];
-    await supabase.storage.from('forms-react').upload(file.name, file);
-    setOutput(JSON.stringify(data, null, 2));
+
+    const formData = new FormData();
+    formData.append('name', userData.name);
+    formData.append('email', userData.email);
+    formData.append('document', userData.document);
+    formData.append('invoiceFile', file);
+
+    try {
+      console.log(userData.name, userData.email, userData.document)
+      const response = await fetch('http://localhost:8080/credit-analyze/analyze-invoice', {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) throw new Error('Erro ao enviar o arquivo para análise');
+
+      const result = await response.json();
+      setOutput(JSON.stringify(result, null, 2));
+    } catch (error) {
+      console.error('Erro ao enviar o arquivo:', error);
+    }
   }
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
