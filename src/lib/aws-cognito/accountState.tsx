b3b9df6bf3ead97eb35a93d8d4797a66f0628a96
diogo_gallina <diogogallina20@gmail.com>
2024-07-31 17:19:54 -0300

    feat: add new variables statement

--- src/lib/aws-cognito/accountState.tsx
+++ src/lib/aws-cognito/accountState.tsx
@@ -1,15 +1,34 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { signUp, authenticate, confirmAccount, resendConfirmationCode } from './helpers';
-import AccountContext from '../../context/accountContext';
+import AccountContext from '../../context/AccountContext';
+import { CognitoUserSession } from 'amazon-cognito-identity-js';
+import userPool from './userPool';
 
 interface AccountStateProps {
   children: ReactNode;
 }
 
 const AccountState: React.FC<AccountStateProps> = ({ children }) => {
+  const [currentSession, setCurrentSession] = useState<CognitoUserSession | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const cognitoUser = userPool.getCurrentUser();
+    if (cognitoUser) {
+      cognitoUser.getSession((err: Error , session: CognitoUserSession | null) =>  {
+    if (!err && session) {
+      setCurrentSession(session);
+      setToken(session.getIdToken().getJwtToken());
+    } else {
+      console.error('Erro ao buscar sessão:', err);
+    }
+  });
+}
+}, []);
+
   return (
     <AccountContext.Provider
-      value={{ signUp, authenticate, confirmAccount, resendConfirmationCode }}
+      value={{ signUp, authenticate, confirmAccount, resendConfirmationCode, currentSession, token }}
     >
       {children}
     </AccountContext.Provider>
