e4ac70105e919f4ed6e38560635e38b4828ad8d7
diogo_gallina <diogogallina20@gmail.com>
2024-08-03 18:47:54 -0300

    feat: create analyze history page

---
+++ src/pages/AnalyzeHistory.tsx
@@ -0,0 +1,9 @@
+import { InvoiceHistory } from '../components/InvoiceHistory';
+
+export default function AnalyzeHistory() {
+  return (
+    <main className='h-screen bg-zinc-50 flex flex-col gap-10 items-center justify-center'>
+      <InvoiceHistory />
+    </main>
+  );
+}
