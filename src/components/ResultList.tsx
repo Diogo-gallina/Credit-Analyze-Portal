bc821dbcca8e8583c691166ae1b0ed3ede150d48
diogo_gallina <diogogallina20@gmail.com>
2024-08-06 16:54:32 -0300

    refactor: change Result List gap

--- src/components/ResultList.tsx
+++ src/components/ResultList.tsx
@@ -17,7 +17,7 @@ interface ResultListProps {
 
 const ResultList: React.FC<ResultListProps> = ({ results, onItemClick }) => (
   <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
-    <nav className="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700">
+    <nav className="flex min-w-[240px] flex-col gap-2 p-2 font-sans text-base font-normal text-blue-gray-700">
       {results.map(result => (
         <ResultCard key={result.id} result={result} onClick={() => onItemClick(result)} />
       ))}
